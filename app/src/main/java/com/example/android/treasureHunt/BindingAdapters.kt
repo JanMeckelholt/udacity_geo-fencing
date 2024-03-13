2a1f09aabcbf7ff3c29a1902e297b59fb5dfec7e
JMeckel <Jan.Meckelholt@mercedes-benz.com>
2024-03-13 14:01:46 +0100

    add BindingAdapters

--- app/src/main/java/com/example/android/treasureHunt/BindingAdapters.kt
+++ app/src/main/java/com/example/android/treasureHunt/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.android.treasureHunt
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import timber.log.Timber
@@ -18,4 +19,19 @@ fun bindTextViewToHintText(textView: TextView, index: Int?) {
     }
     Timber.i("hint: ${context.getString(geofenceHintResourceId)} - index: $index")
     textView.text = context.getString(geofenceHintResourceId)
+}
+
+
+@BindingAdapter("hintImage")
+fun bindImageViewToHintImage(imageView: ImageView, index: Int?) {
+    if (index==null){
+        imageView.setImageResource(R.drawable.android_map)
+        return
+    }
+    val geofenceImageResourceId = when {
+        index < GeofencingConstants.NUM_LANDMARKS -> R.drawable.android_map
+        else -> R.drawable.android_treasure
+    }
+    Timber.i("hint-imgResourceId: $geofenceImageResourceId - img-index: $index")
+    imageView.setImageResource(geofenceImageResourceId)
 }
